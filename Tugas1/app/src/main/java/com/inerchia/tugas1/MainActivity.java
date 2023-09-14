6d9a23b2b8575cc9ff57d9af2a9dc41992199313
Zahra Fayyadiyati <zahra.fayyadiyati@gmail.com>
2023-09-14 09:56:27 +0700

    tugas 1

---
+++ Tugas1/app/src/main/java/com/inerchia/tugas1/MainActivity.java
@@ -0,0 +1,74 @@
+package com.inerchia.tugas1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edtPanjang, edtLebar;
+    private Button btnHitung;
+    private TextView txtLuas;
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtPanjang = (EditText)findViewById(R.id.edt_panjang);
+        edtLebar = (EditText) findViewById(R.id.edt_lebar);
+        btnHitung = (Button)findViewById(R.id.btn_hitung);
+        txtLuas = (TextView) findViewById(R.id.txt_luas);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Hitung Luas Persegi Panjang");
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String panjang = edtPanjang.getText().toString().trim();
+                String lebar = edtLebar.getText().toString().trim();
+
+                double p = Double.parseDouble(panjang);
+                double l = Double.parseDouble(lebar);
+
+                double luas = p * l;
+
+                txtLuas.setText("Luas: " + luas);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
